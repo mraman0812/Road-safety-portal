e3d8a94d2c4e37964a41ef5464be2406bee37384
mraman0812 <mr.aman0812@gmail.com>
2026-02-23 18:05:04 +0530

    Initial Road Safety Portal Upload

---
+++ drivewise/script.js
@@ -0,0 +1,8 @@
+function pledge(){
+    alert("Thank you for pledging to follow road safety rules.");
+}
+
+document.getElementById("feedbackForm").addEventListener("submit", function(e){
+    e.preventDefault();
+    document.getElementById("thankYouMsg").style.display = "block";
+});
